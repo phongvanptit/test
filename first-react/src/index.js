34089f827ef52ff5771f22fa19879edbe8d3a133
phongvanptit <phongvanptit@gmail.com>
2024-06-12 14:46:52 +0700

    update

--- first-react/src/index.js
+++ first-react/src/index.js
@@ -5,23 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAezDT3xfDWTNykFbDg5LO_2enqR-xjKr4",
-  authDomain: "first-project-16bb8.firebaseapp.com",
-  projectId: "first-project-16bb8",
-  storageBucket: "first-project-16bb8.appspot.com",
-  messagingSenderId: "744422541253",
-  appId: "1:744422541253:web:7ce3f6ebe4fb1dc81d2dac"
-};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
